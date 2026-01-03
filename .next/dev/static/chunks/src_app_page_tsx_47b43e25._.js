44889d0974889d57729a3afd9eea65986d5ca722
jettybay <ojetb1@yahoo.com>
2026-01-04 00:27:25 +0100

    Ads spining

--- .next/dev/static/chunks/src_app_page_tsx_47b43e25._.js
+++ .next/dev/static/chunks/src_app_page_tsx_47b43e25._.js
@@ -4,11 +4,11 @@
   "static/chunks/src_750d98e2._.js",
   "static/chunks/node_modules_next_ec1c52de._.js",
   "static/chunks/node_modules_motion-dom_dist_es_da948acf._.js",
-  "static/chunks/node_modules_framer-motion_dist_es_38c80ce2._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_247c3ff9._.js",
   "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_56b6fd65._.js",
   "static/chunks/node_modules_react-icons_fa_index_mjs_0459ff00._.js",
   "static/chunks/node_modules_react-icons_lib_844c6c50._.js",
-  "static/chunks/node_modules_b2fd1472._.js"
+  "static/chunks/node_modules_b8ea8c4f._.js"
 ],
     source: "dynamic"
 });
